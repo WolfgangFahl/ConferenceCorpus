48d8209d0fb0401c30d1f926f91ecc8acfbca9f2
Wolfgang Fahl <wf@bitplan.com>
2021-07-31 19:01:19 +0200

    adds try/catch with WikiCFP down handling

--- tests/test_WikiCfpScrape.py
+++ tests/test_WikiCfpScrape.py
@@ -19,6 +19,7 @@ class TestWikiCFP(unittest.TestCase):
     def setUp(self):
         self.debug=True
         self.profile=True
+        self.wikiCFPDown=True
         pass
 
     def tearDown(self):
@@ -101,11 +102,14 @@ class TestWikiCFP(unittest.TestCase):
         eventIds=[3862,1]
         isDeleted=[False,True]
         event=WikiCfpEventFetcher(debug=self.debug)
-        for index,eventId in enumerate(eventIds):
-            rawEvent=event.fromEventId(eventId)
-            if self.debug:
-                print (rawEvent)
-            self.assertTrue(isDeleted[index]==rawEvent['deleted'])
+        try:
+            for index,eventId in enumerate(eventIds):
+                rawEvent=event.fromEventId(eventId)
+                if self.debug:
+                    print (rawEvent)
+                self.assertTrue(isDeleted[index]==rawEvent['deleted'])
+        except Exception as ex:
+            self.handleError(ex)    
             
     def testGettingEventSeriesForEvent(self):
         '''
@@ -115,15 +119,18 @@ class TestWikiCFP(unittest.TestCase):
         expectedSeriesId=['1769',None]
         eventIds=[1974,139964]
         event=WikiCfpEventFetcher(debug=self.debug,timeout=3.5)
-        for index,eventId in enumerate(eventIds):
-            rawEvent=event.fromEventId(eventId)
-            expected=expectedSeriesId[index]
-            if expected:
-                self.assertEqual(expected,rawEvent['seriesId'])
-            else:
-                self.assertTrue('seriesId' not in rawEvent)
-            if self.debug:
-                print (f"{index}:{rawEvent}")
+        try:
+            for index,eventId in enumerate(eventIds):
+                rawEvent=event.fromEventId(eventId)
+                expected=expectedSeriesId[index]
+                if expected:
+                    self.assertEqual(expected,rawEvent['seriesId'])
+                else:
+                    self.assertTrue('seriesId' not in rawEvent)
+                if self.debug:
+                    print (f"{index}:{rawEvent}")
+        except Exception as ex:
+            self.handleError(ex)
             
     def testGettingLatestEvent(self):
         '''
@@ -140,27 +147,37 @@ class TestWikiCFP(unittest.TestCase):
             if self.debug:
                 print(crawlType.urlPrefix)
             self.assertTrue(crawlType.urlPrefix.endswith("="))
+    
+    def handleError(self,ex):
+        if self.wikiCFPDown and "timed out" in str(ex):
+            print("WikiCFP is down and we can't do anything about it")
+        else:
+            self.fail(f"{str(ex)}")
 
     def testCrawlEvents(self):
         '''
         test crawling a few events and storing the result to a json file
         '''
-        wikiCfpScrape=WikiCfpScrape()
-        limit=10
-        for crawlType in [CrawlType.SERIES]:
-            jsonFilePath=wikiCfpScrape.crawl(0, 1, limit,crawlType)
-            size=os.stat(jsonFilePath).st_size
-            print (f"JSON file for {crawlType.value} has size {size}")
-            self.assertTrue(size>5000)
-            if crawlType is crawlType.EVENT:
-                batchEm=wikiCfpScrape.getEventManager(mode='json')
-                batchEm.fromStore(cacheFile=jsonFilePath)
-                self.assertEqual(len(batchEm.events.values()),limit)
-            inspect=False # if setting to True make sure tmp is on same filesystem
-            # see https://stackoverflow.com/questions/42392600/oserror-errno-18-invalid-cross-device-link
-            if inspect:
-                tmpPath="/tmp/%s" % os.path.basename(jsonFilePath)
-                Path(jsonFilePath).rename(tmpPath)
+        try: 
+            wikiCfpScrape=WikiCfpScrape()
+            limit=10
+            for crawlType in [CrawlType.SERIES]:
+                jsonFilePath=wikiCfpScrape.crawl(0, 1, limit,crawlType)
+                size=os.stat(jsonFilePath).st_size
+                print (f"JSON file for {crawlType.value} has size {size}")
+                self.assertTrue(size>5000)
+                if crawlType is crawlType.EVENT:
+                    batchEm=wikiCfpScrape.getEventManager(mode='json')
+                    batchEm.fromStore(cacheFile=jsonFilePath)
+                    self.assertEqual(len(batchEm.events.values()),limit)
+                inspect=False # if setting to True make sure tmp is on same filesystem
+                # see https://stackoverflow.com/questions/42392600/oserror-errno-18-invalid-cross-device-link
+                if inspect:
+                    tmpPath="/tmp/%s" % os.path.basename(jsonFilePath)
+                    Path(jsonFilePath).rename(tmpPath)
+        except Exception as ex:
+            self.handleError(ex)
+            
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
