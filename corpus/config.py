c35b1c8c5e05e1ea72b8217ce6762f286bc81e43
Wolfgang Fahl <wf@bitplan.com>
2021-08-05 17:07:09 +0200

    adds config module

---
+++ corpus/config.py
@@ -0,0 +1,38 @@
+'''
+Created on 2021-08-05
+
+@author: wf
+'''
+
+class EventDataSourceConfig(object):
+    '''
+    holds configuration parameters for an EventDataSource
+    '''
+    def __init__(self,lookupId:str,name:str,title:str,url:str,tableSuffix:str):
+        '''
+        constructor 
+        
+        Args:
+          lookupId(str): the id of the data source
+          name(str): the name of the data source
+          title(str): the title of the data source
+          url(str): the link to the data source homepage
+          tableSuffix(str): the tableSuffix to use
+        '''  
+        self.lookupId=lookupId
+        self.name=name
+        self.title=title
+        self.url=url
+        self.tableSuffix=tableSuffix
+        
+    def getTableName(self,entityName:str):
+        '''
+        Args:
+            entityName(str): the name of the entity
+        Return:
+            str: the tablename
+        '''
+        tableName=f"{entityName}_{self.tableSuffix}"
+        return tableName
+   
+        
