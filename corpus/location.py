41fcef65baf4b60d4af50fc786fe67e0ab0f7848
Wolfgang Fahl <wf@bitplan.com>
2021-08-12 21:17:47 +0200

    adds more location fixes

--- corpus/location.py
+++ corpus/location.py
@@ -27,6 +27,15 @@ class LocationLookup:
         "Los Angeles California": "Q65",
         "Zurich, Switzerland": "Q72",
         "Barcelona Spain": "Q1492",
+        "Vienna Austria": "Q1741",
+        "Seoul Republic of Korea": "Q8684",
+        "Seattle WA USA": "Q5083",
+        "Singapore Singapore":"Q334",
+        "Tokyo Japan": "Q1490",
+        "Vancouver BC Canada": "Q24639",
+        "Amsterdam Netherlands":"Q727",
+        "Paris France": "Q90",
+        "Nagoya": "Q11751"
     }
 
     def __init__(self):
