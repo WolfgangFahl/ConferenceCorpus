b6f514912e3f270b8070fb968dd008237b0ae6b5
Wolfgang Fahl <wf@bitplan.com>
2021-11-06 16:49:54 +0100

    replace wikidata access library with wikibaseintegrator

--- tests/testWikidataIntegrator.py
+++ tests/testWikidataIntegrator.py
@@ -4,10 +4,10 @@ Created on 2021-10-24
 @author: wf
 '''
 import unittest
-from wikidataintegrator import wdi_core
+from wikibaseintegrator import wbi_core, wbi_login, wbi_datatype,wbi_functions
 import json
 
-class TestWikidataIntegrator(unittest.TestCase):
+class TestWikiBaseIntegrator(unittest.TestCase):
     '''
     test access to Wikidata
     '''
@@ -19,12 +19,14 @@ class TestWikidataIntegrator(unittest.TestCase):
         '''
         test Q5 access
         '''
-        q5 = wdi_core.WDItemEngine(wd_item_id='Q5')
-
-        # to check successful installation and retrieval of the data, you can print the json representation of the item
-        q5dict=q5.get_wd_json_representation()
-        
-        self.prettyJson(json.dumps(q5dict))
+        query = {
+    'action': 'query',
+    'prop': 'revisions',
+    'titles': 'Q5',
+    'rvlimit': 10
+}
+        result=wbi_functions.mediawiki_api_call_helper(query, allow_anonymous=True)
+        self.prettyJson(json.dumps(result))
 
 
 if __name__ == "__main__":
