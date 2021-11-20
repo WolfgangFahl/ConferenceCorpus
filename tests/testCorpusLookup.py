034d51eda52f8c925f0b1a207b75c1fd877f92da
Wolfgang Fahl <wf@bitplan.com>
2021-11-20 10:01:02 +0100

    sets expected number of storageTables to 22

--- tests/testCorpusLookup.py
+++ tests/testCorpusLookup.py
@@ -82,7 +82,7 @@ class TestCorpusLookup(DataSourceTest):
         lookup=CorpusLookup(configure=self.configureCorpusLookup)
         lookup.load()
         storageTableList=EventStorage.getTableList()
-        self.assertEqual(20,len(storageTableList))
+        self.assertEqual(22,len(storageTableList))
         for baseEntity in ["Event","EventSeries"]:
             plantUml=lookup.asPlantUml(baseEntity)
             debug=self.debug
