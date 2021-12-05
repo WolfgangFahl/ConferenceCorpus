729af9bf30bc50f2de7b930d2c226d7671857260
Wolfgang Fahl <wf@bitplan.com>
2021-12-05 17:04:20 +0100

    improves gnd title parsing

--- corpus/datasources/gnd.py
+++ corpus/datasources/gnd.py
@@ -6,12 +6,14 @@ Created on 2020-09-15
 from corpus.eventcorpus import EventDataSource, EventDataSourceConfig
 from corpus.event import EventStorage,EventSeriesManager, EventSeries, Event, EventManager
 from lodstorage.storageconfig import StorageConfig
+import re
 
 class GND(EventDataSource):
     '''
     manages event data from Gemeinsame Normdatei
     https://d-nb.info/standards/elementset/gnd
     '''
+    debug=False
     host="confident.dbis.rwth-aachen.de"
     endpoint=f"https://{host}/jena/gnd/sparql"
     limit=1000000
@@ -33,6 +35,75 @@ class GndEvent(Event):
         samples = [      
         ]
         return samples
+    
+    def setOrganization(self,orgStr):
+        self.organization=orgStr
+    
+    def setLocation(self,location):
+        self.location=location
+        
+    def setOrdinal(self,ordinalStr,counter):
+        ordinalStr=ordinalStr.strip()
+        m=re.match(r"^([0-9]+)\.?$",ordinalStr)
+        if m:
+            ordinal=int(m.group(1))
+            self.ordinal=ordinal
+            pass
+        else:
+            if GND.debug:
+                print(f"ordinal?:{ordinalStr}")
+            counter["invalidOrdinal"]+=1
+    
+    def setYear(self,yearStr,counter):
+        '''
+        set the year of the given event
+        '''
+        yearStr=yearStr.strip()
+        if re.match(r"^[0-9]+$",yearStr):
+            self.year=int(yearStr)
+            return True
+        else: 
+            m=re.match(r"^([0-9]+)\s*\-\s*([0-9]+)$",yearStr)
+            if m:
+                self.year=int(m.group(1))
+                counter["yearRange"]+=1
+                return True
+            else:
+                if GND.debug:
+                    print(f"year?:{yearStr}")
+                counter["invalidYear"]+=1
+                return False
+        
+    def titleExtract(self,counter):
+        '''
+        extract meta data information from the title of this gnd event
+        '''
+        regex=r"(.*?)\((.*?)\)"
+        m=re.match(regex,self.title)
+        if m:
+            self.title=m.group(1)
+            ordYearLocation=m.group(2)
+            parts=ordYearLocation.split(":")
+            plen=len(parts)
+            counter[plen]+=1
+            if plen==1:
+                self.setYear(ordYearLocation,counter)
+            elif plen==2:
+                self.setYear(parts[0],counter)
+                self.setLocation(parts[1])
+            elif plen==3:
+                self.setOrdinal(parts[0],counter)
+                self.setYear(parts[1],counter)
+                self.setLocation(parts[2])
+            elif plen==4:
+                self.setOrganization(parts[0])
+                self.setOrdinal(parts[1],counter)
+                self.setYear(parts[2],counter)
+                self.setLocation(parts[3])
+                pass
+        else:
+            #print(event.title)
+            counter["invalid"]+=1
       
 class GndEventSeries(EventSeries):
     '''
--- tests/testGnd.py
+++ tests/testGnd.py
@@ -9,7 +9,6 @@ from corpus.lookup import CorpusLookup
 from corpus.event import EventStorage
 from corpus.datasources.gnd import GND
 from collections import Counter
-import re
 from lodstorage.query import Query
 import getpass
 import subprocess, platform
@@ -55,80 +54,21 @@ class TestGnd(DataSourceTest):
         expected=min(GND.limit,700000)
         self.checkDataSource(gndDataSource,1,expected)
         pass
+
     
-    def setOrganization(self,event,orgStr):
-        event.organization=orgStr
-    
-    def setLocation(self,event,location):
-        event.location=location
-        
-    def setOrdinal(self,event,ordinalStr,counter):
-        ordinalStr=ordinalStr.strip()
-        m=re.match(r"^([0-9]+)\.?$",ordinalStr)
-        if m:
-            ordinal=int(m.group(1))
-            event.ordinal=ordinal
-            pass
-        else:
-            counter["invalidOrdinal"]+=1
-    
-    def setYear(self,event,yearStr,counter):
-        '''
-        set the year of the given event
-        '''
-        yearStr=yearStr.strip()
-        if re.match(r"^[0-9]+$",yearStr):
-            event.year=int(yearStr)
-            return True
-        else:
-            if self.debug:
-                print(yearStr)
-            counter["invalidYear"]+=1
-            return False
-        
-    
-    def gndTitleExtract(self,event,counter):
-        '''
-        extract title information from gnd event
-        '''
-        regex=r"(.*)\((.*?)\)"
-        m=re.match(regex,event.title)
-        if m:
-            event.title=m.group(1)
-            ordYearLocation=m.group(2)
-            parts=ordYearLocation.split(":")
-            plen=len(parts)
-            counter[plen]+=1
-            if plen==1:
-                self.setYear(event,ordYearLocation,counter)
-            elif plen==2:
-                self.setYear(event,parts[0],counter)
-                self.setLocation(event,parts[1])
-            elif plen==3:
-                self.setOrdinal(event,parts[0],counter)
-                self.setYear(event,parts[1],counter)
-                self.setLocation(event,parts[2])
-            elif plen==4:
-                self.setOrganization(event,parts[0])
-                self.setOrdinal(event,parts[1],counter)
-                self.setYear(event,parts[2],counter)
-                self.setLocation(event,parts[3])
-                pass
-        else:
-            #print(event.title)
-            counter["invalid"]+=1
-            
-    
-    def testOrdinal(self):
+    def testTitleExtract(self):
         '''
         test ordinal analysis
         '''
+        oldDebug=GND.debug
+        GND.debug=True
         gndDataSource=self.getGndDataSource()
         events=gndDataSource.eventManager.events
         counter=Counter()
         for event in events:
-            self.gndTitleExtract(event,counter)
+            event.titleExtract(counter)
         print (counter.most_common())
+        GND.debug=oldDebug
         
     def testStats(self):
         '''
