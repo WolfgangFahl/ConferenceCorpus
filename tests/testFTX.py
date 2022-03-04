e1bbff8055f36dde4ed246ff87a8a1fff4b6fe2f
Wolfgang Fahl <wf@bitplan.com>
2022-03-04 14:31:32 +0100

    adds parseAll tests and improves mapping

--- tests/testFTX.py
+++ tests/testFTX.py
@@ -7,6 +7,7 @@ from tests.datasourcetoolbox import DataSourceTest
 import getpass
 from corpus.datasources.tibkat import FTXParser
 from corpus.xml.xmlparser import XmlEntity
+import sys
 
 class TestFTXParser(DataSourceTest):
     '''
@@ -22,18 +23,25 @@ class TestFTXParser(DataSourceTest):
             self.ftxroot="/Volumes/seel/tibkat-ftx/tib-intern-ftx_0/tib-2021-12-20"
         if self.ftxroot is not None:
             self.ftxParser=FTXParser(self.ftxroot)
-        self.sampleFtx="tib-intern-ftx_2021-12-20_T201424_5766.xml"
-            
+        self.sampleBase="tib-intern-ftx_2021-12-20_"
+        self.samples=["T184133_3056","T201424_5766","T184335_3113","T182353_2562"]
+        self.sampleFtxs=[]
+        for sample in self.samples:
+            self.sampleFtxs.append(f"{self.sampleBase}{sample}.xml")
+    
+    def getAllFtxXmlFiles(self):
+        if not hasattr(self, "xmlFiles"):
+            self.xmlFiles=self.ftxParser.ftxXmlFiles()
+            if self.debug:
+                print(f"found {len(self.xmlFiles)} FTX files")
+        return self.xmlFiles
+        
     def testFtxXmlFiles(self):
         '''
         check listing the FTX xml files
         '''
-        debug=self.debug
-        #debug=True
         if self.ftxParser is not None:
-            self.xmlFiles=self.ftxParser.ftxXmlFiles()
-            if debug:
-                print(f"found {len(self.xmlFiles)} FTX files")
+            self.getAllFtxXmlFiles()
             self.assertTrue(len(self.xmlFiles)>7600)
             
     def testDocumentParsing(self):
@@ -41,22 +49,28 @@ class TestFTXParser(DataSourceTest):
         test parsing documents out of the FTX xml file
         '''
         debug=self.debug
-        debug=True
-        show=True
+        #debug=True
+        #show=True
+        show=False
         XmlEntity.debug=debug
         if self.ftxParser is not None:
-            xmlPath=f"{self.ftxroot}/{self.sampleFtx}"
             documents={}
-            for document in self.ftxParser.parse(xmlPath):
+            for sampleFtx in self.sampleFtxs:
+                xmlPath=f"{self.ftxroot}/{sampleFtx}"
+                for document in self.ftxParser.parse(xmlPath):
+                    if debug:
+                        print(document)
+                    ppn=document.ppn
+                    documents[ppn]=document
                 if debug:
-                    print(document)
-                documents[document.ppn]=document
-            if debug:
-                print (f"found {len(documents)} documents")    
-            self.assertEqual(100,len(documents))
+                    print (f"found {len(documents)} documents in {sampleFtx}")    
+            self.assertEqual(100*len(self.sampleFtxs),len(documents))
             ppns=[
                 "579171965", # ISWC 2008
                 #"578841517" # SoftVis  2008
+                #"1662974825" # SafeAI 2019
+                "1677843861", # AAAI 2018
+                "668314257" #
             ]
             for ppn in ppns:
                 self.assertTrue(ppn in documents)
@@ -65,4 +79,25 @@ class TestFTXParser(DataSourceTest):
                     print(document.rawxml)
                 if show:
                     print(document)
+                    
+    def testParseAll(self):
+        '''
+        test parse all ftx files
+        '''
+        if self.ftxParser is not None:
+            self.getAllFtxXmlFiles()
+            count=0
+            for xmlFile in self.xmlFiles:
+                xmlPath=f"{self.ftxroot}/{xmlFile}"
+                for document in self.ftxParser.parse(xmlPath):
+                    count+=1
+                    if count%1000==0:
+                        print(".",end="")
+                    if count%10000==0:
+                        print(f"{count}",end="")
+                    if count%80000==0:
+                        print()    
+                    
+                
+        
             
