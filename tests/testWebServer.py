ca7fbd79d6df6cdc62d64a1045987c323233f826
Wolfgang Fahl <wf@bitplan.com>
2022-04-04 06:54:14 +0200

    improves unused variable handling

--- tests/testWebServer.py
+++ tests/testWebServer.py
@@ -73,7 +73,7 @@ class TestWebServer(DataSourceTest):
         tests the completion of an scholar over wikidata
         """
         expectedScholars=Scholar.getSamples()
-        ws, app, client=self.getApp()
+        _ws, _app, client=self.getApp()
         data=[{"wikiDataId":"Q54303353"}]
         res=client.post('/scholar/complete', data=json.dumps(data))
         self.assertDictEqual(expectedScholars[0], res.json[0])
