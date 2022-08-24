32959ca54e734c2cbe367d2f0425ff8e338302c8
tholzheim <tim.holzheim@rwth-aachen.de>
2022-08-24 18:21:28 +0200

    prepares v0.0.24

--- corpus/version.py
+++ corpus/version.py
@@ -8,8 +8,8 @@ class Version(object):
     '''
     Version handling for ConferenceCorpus
     '''
-    version='0.0.33'
+    version='0.0.34'
     date = '2020-09-10'
-    updated = '2022-04-10'
+    updated = '2022-08-24'
 
         
