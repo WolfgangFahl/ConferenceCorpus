e9162d4d24ab404ac370699fa95defe2a8f5a2fe
Wolfgang Fahl <wf@bitplan.com>
2022-12-21 19:49:03 -0700

    prepares release

--- corpus/version.py
+++ corpus/version.py
@@ -8,8 +8,8 @@ class Version(object):
     '''
     Version handling for ConferenceCorpus
     '''
-    version='0.1.0'
+    version='0.1.1'
     date = '2020-09-10'
-    updated = '2022-11-20'
+    updated = '2022-12-21'
 
         
