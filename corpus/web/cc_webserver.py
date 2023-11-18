573d0ca2fa88517b321acd2a7541d251f2073841
Wolfgang Fahl <wf@bitplan.com>
2023-11-18 15:07:14 +0100

    adds list of datasources

--- corpus/web/cc_webserver.py
+++ corpus/web/cc_webserver.py
@@ -6,6 +6,9 @@ Created on 2023-11-18
 from ngwidgets.input_webserver import InputWebserver
 from ngwidgets.webserver import WebserverConfig
 from corpus.version import Version
+from nicegui import ui, Client
+from ngwidgets.profiler import Profiler
+from corpus.eventcorpus import DataSource
 
 class ConferenceCorpusWebserver(InputWebserver):
     """
@@ -27,3 +30,25 @@ class ConferenceCorpusWebserver(InputWebserver):
         """Constructor"""
         InputWebserver.__init__(self, config=ConferenceCorpusWebserver.get_config())
  
+    def setup_home(self):
+        """
+        first load all data sources then
+        show a table of these
+        """
+        msg="loading datasources ..."
+        self.loading_msg=ui.html(msg)
+        profiler=Profiler(msg,profile=False)
+        DataSource.getAll()
+        elapsed=profiler.time()
+        data_sources=DataSource.sources.values()
+        msg=f"{len(data_sources)} datasources loaded in {elapsed*1000:5.0f} msecs"
+        self.loading_msg.content=msg
+        for index,source in enumerate(data_sources,start=1):
+            ui.label(f"{index}:{source.title}")
+        pass
+        
+    async def home(self, _client: Client):
+        """
+        provide the main content page
+        """
+        await(self.setup_content_div(self.setup_home))
